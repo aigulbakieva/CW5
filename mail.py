4ee56190e0401733f3d44d6e712dce83a56bcf5e
aigulbakieva <bakisha.a@mail.ru>
2023-11-02 13:01:20 +0100

    added user interface

--- mail.py
+++ mail.py
@@ -16,12 +16,32 @@ def main():
     save_vacancies_to_db(vacancies, 'headhunter', params)
 
     db_manager = DBManager('headhunter', params)
+    while True:
+        print('''
+        Добрый день!
+        Выберите один из пунктов:
+        1 - получить список всех вакансий и количество вакансий у каждой компании
+        2 - получить cписок всех вакансий с указанием всей информации о вакансии
+        3 - получить среднюю зарплату по всем вакансиям 
+        4 - получить вакансии, у которых зарплата выше средней по всем вакансиям 
+        5 - получить список вакансий, в названии которых содержится слово Python
+        0 - выход''')
 
-    db_manager.get_companies_and_vacancies_count()
-    db_manager.get_all_vacancies()
-    db_manager.get_avg_salary()
-    db_manager.get_vacancies_with_higher_salary()
-    db_manager.get_vacancies_with_keyword()
+        user_input = input()
+        if user_input == '1':
+            db_manager.get_companies_and_vacancies_count()
+        elif user_input == '2':
+            db_manager.get_all_vacancies()
+        elif user_input == '3':
+            db_manager.get_avg_salary()
+        elif user_input == '4':
+            db_manager.get_vacancies_with_higher_salary()
+        elif user_input == '5':
+            db_manager.get_vacancies_with_keyword()
+        elif user_input == '0':
+            break
+        else:
+            print('Неверный запрос')
 
 
 if __name__ == '__main__':
